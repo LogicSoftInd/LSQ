cb1fcd88c8237dbf482a3c48e7a57cf730520b91
Mike Shultz <mike@votesmart.org>
2015-07-02 22:29:05 -0600

    Database basics added(except delete). Filler for run_query added, but not yet functional.

--- db.py
+++ db.py
@@ -5,7 +5,8 @@ import config
 
 client = m.MongoClient(config.mongo_hostname, config.mongo_port)
 db = client[config.mongo_db]
-queries = db[config.mongo_collection]
+queries = db[config.query_mongo_collection]
+databases = db[config.database_mongo_collection]
 
 def get_tags_list(tags):
     return [ tag.strip() for tag in tags.strip().split(",") \
@@ -42,3 +43,31 @@ def get_queries():
 
 def get_query_details(id):
     return queries.find_one(ObjectId(id))
+
+def insert_database(name, hostname, port, desc):
+    databases.insert({
+            "name": name,
+            "hostname": hostname,
+            "port": port,
+            "desc": desc
+        })
+
+def update_database(id, name, hostname, port, desc):
+    databases.update({
+        "_id": ObjectId(id)
+        },
+        { "$set" : {
+                "name": name,
+                "hostname": hostname,
+                "port": port,
+                "desc": desc
+            }}, upsert=False)
+
+def delete_database(id):
+    databases.remove({"_id": ObjectId(id)})
+
+def get_databases():
+    return list(databases.find())
+
+def get_database_details(id):
+    return databases.find_one(ObjectId(id))
